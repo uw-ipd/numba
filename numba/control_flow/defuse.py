d975dd8550125598f8dd11a9ccf52783ee01b5d3
Mark Florisson <markflorisson88@gmail.com>
2013-03-27 16:07:58 +0000

    Start on separate def/use chainer (eliminate symbol table)

---
+++ numba/control_flow/defuse.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+Create def/use chains and vice versa.
+
+A definition is:
+
+    * A variable assignment instance ('x = 0')
+    * A phi at a control flow merge point
+
+A use is a variable read:
+
+    'x'
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import ast
+
+from numba import nodes
+from numba import symtab
+from numba import visitors
+from numba.nodes import cfnodes
+
+#------------------------------------------------------------------------
+# Def/use chainer
+#------------------------------------------------------------------------
+
+class DefUseChainer(visitors.NumbaTransformer):
+    """
+    Merge PHIs into AST. This happens after the CFG was build and the
+    phis computed.
+    """
--- numba/control_flow/ssa.py
+++ numba/control_flow/ssa.py
@@ -239,6 +239,54 @@ class SSAifier(object):
                 stat.node.variable = current_var
                 current_var.cf_references.append(stat.node)
 
+#------------------------------------------------------------------------
+# Kill unused Phis
+#------------------------------------------------------------------------
+
+def kill_phi(block, phi):
+    logger.debug("Killing phi: %s", phi)
+
+    block.symtab.pop(phi.variable.renamed_name)
+
+    for incoming_var in phi.incoming:
+        # A single definition can reach a block multiple times,
+        # remove all references
+        refs = [ref for ref in incoming_var.cf_references
+                        if ref.variable is not phi.variable]
+        incoming_var.cf_references = refs
+
+def kill_unused_phis(cfg):
+    """
+    Kill phis right after computing the SSA graph.
+
+    Kills phis which are not referenced. We need to do this bottom-up,
+    i.e. in reverse topological dominator-tree order, since in SSA
+    a definition always lexically precedes a reference.
+
+    This is important, since it kills any unnecessary promotions (e.g.
+    ones to object, which LLVM wouldn't be able to optimize out).
+
+    TODO: Kill phi cycles, or do reachability analysis before inserting phis.
+    """
+    changed = False
+
+    for block in cfg.blocks[::-1]:
+        phi_nodes = []
+
+        for i, phi in enumerate(block.phi_nodes):
+            if phi.variable.cf_references:
+                # Used phi
+                # logger.info("Used phi %s, %s" % (phi, phi.variable.cf_references))
+                phi_nodes.append(phi)
+            else:
+                # Unused phi
+                changed = True
+
+                kill_phi(block, phi)
+
+        block.phi_nodes = phi_nodes
+
+    return changed
 
 #------------------------------------------------------------------------
 # Merge Phis into AST (CFA stage)
@@ -317,54 +365,6 @@ class PhiInjector(visitors.NumbaTransformer):
         return ast.Suite(preceding + [for_node] + postceding)
 
 
-#------------------------------------------------------------------------
-# Kill unused Phis
-#------------------------------------------------------------------------
-
-def kill_phi(block, phi):
-    logger.debug("Killing phi: %s", phi)
-
-    block.symtab.pop(phi.variable.renamed_name)
-
-    for incoming_var in phi.incoming:
-        # A single definition can reach a block multiple times,
-        # remove all references
-        refs = [ref for ref in incoming_var.cf_references
-                        if ref.variable is not phi.variable]
-        incoming_var.cf_references = refs
-
-def kill_unused_phis(cfg):
-    """
-    Used before running type inference.
-
-    Kill phis which are not referenced. We need to do this bottom-up,
-    i.e. in reverse topological dominator-tree order, since in SSA
-    a definition always lexically precedes a reference.
-
-    This is important, since it kills any unnecessary promotions (e.g.
-    ones to object, which LLVM wouldn't be able to optimize out).
-
-    TODO: Kill phi cycles, or do reachability analysis before inserting phis.
-    """
-    changed = False
-
-    for block in cfg.blocks[::-1]:
-        phi_nodes = []
-
-        for i, phi in enumerate(block.phi_nodes):
-            if phi.variable.cf_references:
-                # Used phi
-                # logger.info("Used phi %s, %s" % (phi, phi.variable.cf_references))
-                phi_nodes.append(phi)
-            else:
-                # Unused phi
-                changed = True
-
-                kill_phi(block, phi)
-
-        block.phi_nodes = phi_nodes
-
-    return changed
 
 #------------------------------------------------------------------------
 # Iterate over all phi nodes or variables

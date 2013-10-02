44e57b805df6554c07c62bb58cf6b5f7c049a9d0
Jay Bourque <jay.bourque@continuum.io>
2013-10-02 14:20:54 -0500

    Refactor tests

--- numba/tests/test_float_or_none.py
+++ numba/tests/test_float_or_none.py
@@ -3,20 +3,20 @@ from __future__ import print_function, division, absolute_import
 import unittest
 from numba import jit, float_, double
 
-def f(x):
+def identity(x):
     return x
 
-def g(x):
+def assign(x):
     if x > 2.0:
         x = None
     return x
 
-def h(x):
+def return_scalar(x):
     if x == None or x > 0.0:
         return x
     return None
 
-def h2(x, y):
+def return_list(x, y):
     first = None if x < 0.0 else x
     second = None if y < 0.0 else y
     return [first, second]
@@ -24,32 +24,50 @@ def h2(x, y):
 class TestFloatOrNone(unittest.TestCase):
 
     def test_float(self):
-        f_float = jit(float_(float_))(f)
-        assert f_float(None) == None
-        assert f_float(2.0) == 2.0
+        identity_float = jit(float_(float_))(identity)
+        assert identity_float(None) == None
+        assert identity_float(2.0) == 2.0
 
     def test_double(self):
-        f_double = jit(double(double))(f)
-        assert f_double(None) == None
-        assert f_double(2.0) == 2.0
+        identity_double = jit(double(double))(identity)
+        assert identity_double(None) == None
+        assert identity_double(2.0) == 2.0
 
-    def test_float_assign_none(self):
-        assign = jit(double(double))(g)
-        assert assign(3.0) == None
-        assert assign(1.0) == 1.0
+    def test_assign_float_none(self):
+        assign_func = jit(float_(float_))(assign)
+        assert assign_func(3.0) == None
+        assert assign_func(1.0) == 1.0
 
-    def test_return_double_or_none(self):
-        h_func = jit(double(double))(h)
-        assert h_func(1.0) == 1.0
-        assert h_func(-1.0) == None
-        assert h_func(None) == None
+    def test_assign_double_none(self):
+        assign_func = jit(double(double))(assign)
+        assert assign_func(3.0) == None
+        assert assign_func(1.0) == 1.0
 
-    def test_return_fixed_len_list_double_or_none(self):
-        h2_func = jit(double[:](double, double))(h2)
-        assert h2_func(2.0, 3.0) == [2.0, 3.0]
-        assert h2_func(2.0, -3.0) == [2.0, None]
-        assert h2_func(-2.0, -3.0) == [None, None]
-        assert h2_func(-2.0, 3.0) == [None, 3.0]
+    def test_return_scalar_float(self):
+        return_scalar_func = jit(float_(float_))(return_scalar)
+        assert return_scalar_func(1.0) == 1.0
+        assert return_scalar_func(-1.0) == None
+        assert return_scalar_func(None) == None
+
+    def test_return_scalar_double(self):
+        return_scalar_func = jit(double(double))(return_scalar)
+        assert return_scalar_func(1.0) == 1.0
+        assert return_scalar_func(-1.0) == None
+        assert return_scalar_func(None) == None
+
+    def test_return_list_float(self):
+        return_list_func = jit(float_[:](float_, float_))(return_list)
+        assert return_list_func(2.0, 3.0) == [2.0, 3.0]
+        assert return_list_func(2.0, -3.0) == [2.0, None]
+        assert return_list_func(-2.0, -3.0) == [None, None]
+        assert return_list_func(-2.0, 3.0) == [None, 3.0]
+
+    def test_return_list_double(self):
+        return_list_func = jit(double[:](double, double))(return_list)
+        assert return_list_func(2.0, 3.0) == [2.0, 3.0]
+        assert return_list_func(2.0, -3.0) == [2.0, None]
+        assert return_list_func(-2.0, -3.0) == [None, None]
+        assert return_list_func(-2.0, 3.0) == [None, 3.0]
 
 if __name__ == '__main__':
     unittest.main()

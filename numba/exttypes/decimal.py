c3d9d3b1bc216e403567ca6b37fe8a497f4558b2
Jay Bourque <jay.bourque@continuum.io>
2013-11-25 11:45:24 -0600

    Move decimal extension type

--- numba/exttypes/decimal.py
+++ numba/exttypes/decimal.py
@@ -1,8 +1,6 @@
 from ctypes import CDLL, Structure, POINTER, c_longlong, c_uint, c_int, \
     c_ubyte, c_char_p, byref, pointer
-from numba import autojit, jit, void, c_string_type, int_, typeof, object_, void
-import cdecimal
-import random
+from numba import jit, void, c_string_type, int_, typeof, object_, void
 
 
 class mpd_context(Structure):
@@ -65,7 +63,7 @@ dll.mpd_init(byref(context))
 
 
 @jit
-class NumbaDecimal(object):
+class Decimal(object):
     
     @void(c_string_type)
     def __init__(self, value):
@@ -82,57 +80,31 @@ class NumbaDecimal(object):
         return result
 
 
-@jit(NumbaDecimal.exttype(NumbaDecimal.exttype, NumbaDecimal.exttype))
+@jit(Decimal.exttype(Decimal.exttype, Decimal.exttype))
 def add(left, right):
     with nopython:
         mpd_result = mpd_new_func()
         mpd_add_func(mpd_result, left.mpd, right.mpd, context_ref)
-    return NumbaDecimal(mpd_to_sci_func(mpd_result, 0))
+    return Decimal(mpd_to_sci_func(mpd_result, 0))
 
-@jit(NumbaDecimal.exttype(NumbaDecimal.exttype, NumbaDecimal.exttype))
+@jit(Decimal.exttype(Decimal.exttype, Decimal.exttype))
 def sub(left, right):
     with nopython:
         mpd_result = mpd_new_func()
         mpd_sub_func(mpd_result, left.mpd, right.mpd, context_ref)
-    return NumbaDecimal(mpd_to_sci_func(mpd_result, 0))
+    return Decimal(mpd_to_sci_func(mpd_result, 0))
 
-@jit(NumbaDecimal.exttype(NumbaDecimal.exttype, NumbaDecimal.exttype))
+@jit(Decimal.exttype(Decimal.exttype, Decimal.exttype))
 def mul(left, right):
     with nopython:
         mpd_result = mpd_new_func()
         mpd_mul_func(mpd_result, left.mpd, right.mpd, context_ref)
-    return NumbaDecimal(mpd_to_sci_func(mpd_result, 0))
+    return Decimal(mpd_to_sci_func(mpd_result, 0))
 
-@jit(NumbaDecimal.exttype(NumbaDecimal.exttype, NumbaDecimal.exttype))
+@jit(Decimal.exttype(Decimal.exttype, Decimal.exttype))
 def div(left, right):
     with nopython:
         mpd_result = mpd_new_func()
         mpd_div_func(mpd_result, left.mpd, right.mpd, context_ref)
-    return NumbaDecimal(mpd_to_sci_func(mpd_result, 0))
+    return Decimal(mpd_to_sci_func(mpd_result, 0))
 
-
-@autojit
-def numba_test(num1, num2):
-    x = NumbaDecimal(num1)
-    y = NumbaDecimal(num2)
-    return mul(x, y)
-
-@autojit
-def python_test(num1, num2):
-    x = cdecimal.Decimal(num1)
-    y = cdecimal.Decimal(num2)
-    return x * y
-
-def benchmark_numba():
-    numba_test(str(random.random()), str(random.random()))
-
-def benchmark_python():
-    python_test(str(random.random()), str(random.random()))
-
-import timeit
-
-timer = timeit.Timer("benchmark_numba()", "from __main__ import benchmark_numba")
-print 'Numba extension type times:', timer.repeat(repeat=3, number=100000)
-
-timer = timeit.Timer("benchmark_python()", "from __main__ import benchmark_python")
-print 'Python cdecimal times:', timer.repeat(repeat=3, number=100000)

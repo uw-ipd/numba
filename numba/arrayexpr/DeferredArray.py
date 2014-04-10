e3a5e0641f66e9e63332a08d441e34a19d9924c0
Jay Bourque <jay.bourque@continuum.io>
2014-04-10 16:34:58 -0500

    Add owners flag and Repr class

--- numba/arrayexpr/DeferredArray.py
+++ numba/arrayexpr/DeferredArray.py
@@ -1,6 +1,7 @@
 import numpy as np
 import operator
 import math
+import weakref
 from pylab import imshow, show
 from alge import Case, of, datatype
 from collections import namedtuple
@@ -9,7 +10,7 @@ from collections import namedtuple
 UnaryOperation = datatype('UnaryOperation', ['operand', 'op', 'op_str'])
 BinaryOperation = datatype('BinaryOperation', ['lhs', 'rhs', 'op', 'op_str'])
 ArrayAssignOperation = datatype('ArrayAssignOperation', ['operand', 'key', 'value'])
-ArrayNode = datatype('ArrayNode', ['data'])
+ArrayNode = datatype('ArrayNode', ['data', 'owners'])
 ArrayDataNode = datatype('ArrayDataNode', ['array_data'])
 ScalarConstantNode = datatype('ScalarConstantNodeNode', ['value'])
 
@@ -45,8 +46,12 @@ class DeferredArray(object):
     def __init__(self, data=None):
         if isinstance(data, np.ndarray):
             data = ArrayDataNode(array_data=data)
-        self.array_node = ArrayNode(data=data)
+        self._ref = weakref.ref(self)
+        self.array_node = ArrayNode(data=data, owners=set([self._ref]))
         self.data = None
+
+    def __del__(self):
+        self.array_node.owners.discard(self._ref)
 
     def __get_data(self):
         if self.data is None:
@@ -55,7 +60,10 @@ class DeferredArray(object):
 
     def __str__(self):
         return str(self.__get_data())
-    
+
+    def __repr__(self):
+        return Repr(self.array_node, state={'level':0})
+
     @binary_op(operator.add, 'operator.add')
     def __add__(self, other):
         pass
@@ -89,7 +97,8 @@ class DeferredArray(object):
             value = value.array_node
         else:
             value = ScalarConstantNode(value)
-        self.array_node = ArrayNode(data=ArrayAssignOperation(self.array_node, key, value))
+        self.array_node = ArrayNode(data=ArrayAssignOperation(self.array_node, key, value),
+                                    owners=set(self._ref))
 
 
 @unary_op(abs, 'abs')
@@ -103,8 +112,8 @@ def numba_log(operand):
 
 class Value(Case):
 
-    @of('ArrayNode(data)')
-    def array_node(self, data):
+    @of('ArrayNode(data, owners)')
+    def array_node(self, data, owners):
         return Value(data)
 
     @of('ArrayDataNode(array_data)')
@@ -129,10 +138,53 @@ class Value(Case):
         return Value(operand)
 
 
+def get_indent(level):
+    return ''.join([' '] * level * 4)
+
+class Repr(Case):
+
+    @of('ArrayNode(data, owners)')
+    def array_node(self, data, owners):
+        level = self.state['level']
+        return '{0}ArrayNode (owned={1}): \n{2}'.format(get_indent(level),
+            str(bool(owners)),
+            str(Repr(data, state={'level':level+1})))
+
+    @of('ArrayDataNode(array_data)')
+    def array_data_node(self, array_data):
+        level = self.state['level']
+        return '{0}array_data: {1}\n'.format(get_indent(level), str(array_data))
+
+    @of('ScalarConstantNode(value)')
+    def scalar_constant(self, value):
+        level = self.state['level']
+        return '{0}value: {1}\n'.format(get_indent(level), str(value))
+
+    @of('UnaryOperation(operand, op, op_str)')
+    def unary_operation(self, operand, op, op_str):
+        level = self.state['level']
+        return '{0}UnaryOperation: \n{1}\n'.format(get_indent(level),
+            Repr(operand, state={'level':level+1}))
+
+    @of('BinaryOperation(lhs, rhs, op, op_str)')
+    def binary_operation(self, lhs, rhs, op, op_str):
+        level = self.state['level']
+        return '{0}BinaryOperation: \n{1}\n{2}\n'.format(get_indent(level),
+            Repr(lhs, state={'level':level+1}),
+            Repr(rhs, state={'level':level+1}))
+
+    @of('ArrayAssignOperation(operand, key, value)')
+    def array_assign_operation(self, operand, key, value):
+        level = self.state['level']
+        return '{0}ArrayAssignOperation: \n{1}\n{2}\n'.format(get_indent(level),
+            Repr(key, state={'level':level+1}),
+            Repr(value, state={'level':level+1}))
+
+
 class CodeGen(Case):
 
-    @of('ArrayNode(data)')
-    def array(self, data):
+    @of('ArrayNode(data, owners)')
+    def array(self, data, owners):
         return CodeGen(data, state=self.state)
 
     @of('ArrayDataNode(array_data)')
@@ -161,17 +213,28 @@ def test1():
 
     result = a1**2 + numba_abs(a2)
 
+    print result.__repr__()
     print result
     print CodeGen(result.array_node, state={'count': 0})
 
 
 def test2():
 
-    a = DeferredArray(data=np.arange(-10,10))
+    a = DeferredArray(data=np.arange(10))
 
     a[:] = a + a
 
     print a
+
+
+def test3():
+
+    a = DeferredArray(data=np.arange(10))
+
+    b = a + a
+    c = b + b
+
+    print c
 
 
 def test_mandelbrot():
@@ -205,5 +268,4 @@ def test_mandelbrot():
 
 if __name__ == '__main__':
     test1()
-    #test2()
 
